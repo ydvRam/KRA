ad099cad037f11cc2ca87cde81bf216458306b68
Ram Pratap <ramyadav8395@gmail.com>
2026-02-24 15:23:13 +0530

    learning javascript

---
+++ Web Fundamentals/javascript/index.js
@@ -0,0 +1,8 @@
+// js variables -->
+let fullName = "Rampratap"; //we can update the let but we cannot re-declare it
+console.log(fullName);
+const PI = 3.24;  //we cannot update or re-declare const
+console.log(PI);
+var age = 25;  //we can update and re-declare var but it is not recommended to use var
+console.log(age);
+
