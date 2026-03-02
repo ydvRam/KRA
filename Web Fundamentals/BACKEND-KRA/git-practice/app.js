6fc4bd6703cbbeafd8c32f850bdcfd5877daa58c
Ram Pratap <ramyadav8395@gmail.com>
2026-03-02 13:03:08 +0530

    chore: move git practice into parent repository

---
+++ Web Fundamentals/BACKEND-KRA/git-practice/app.js
@@ -0,0 +1,11 @@
+console.log("Hello Git");
+console.log("Hello from Master Branch");
+function login() {
+  console.log("User logged in");
+}
+
+login();
+console.log("Hello from features.signup branch");
+function signup(){
+  console.log("User Sign up...");
+}
