902dddde5d90fad18e8414bc16f25741f600307f
Ram Pratap <ramyadav8395@gmail.com>
2026-03-07 09:47:13 +0530

    reset db configuration

--- Web Fundamentals/BACKEND-KRA/postgreSQL-basic/src/config/db.js
+++ Web Fundamentals/BACKEND-KRA/postgreSQL-basic/src/config/db.js
@@ -1,11 +1,22 @@
 const { Pool } = require("pg");
+
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
+  max: 10, // max connections
+  idleTimeoutMillis: 30000, // close idle connection after 30s
+  connectionTimeoutMillis: 2000 // timeout if cannot connect
 });
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD TYPE:", typeof process.env.DB_PASSWORD);
-module.exports= pool;
+
+pool.on("connect", () => {
+  console.log("PostgreSQL connected");
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
+});
+
+module.exports = pool;
